2dbaa8182c85c0903fc42b9273760ede765c0edb
sanitkorn <sanitkorn112233@gmail.com>
2020-01-02 11:13:27 +0700

    firt commit

---
+++ resources/views/mobiles.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Mobile Store</title>
+</head>
+<body>
+    <h1>Mobile</h1>
+</body>
+</html>
---
+++ resources/views/user.blade.php
@@ -0,0 +1,2 @@
+<h1>User</h1>
+<a href="/"Hello world from:><?php echo $user ?></a>
